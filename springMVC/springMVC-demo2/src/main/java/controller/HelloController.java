1c77168e205417f4e7933d9b7bcf8ab12afd7970
Cindy <siyue.Chen1997@gmail.com>
2024-04-22 00:25:04 +0800

    requestheader

--- springMVC/springMVC-demo2/src/main/java/controller/HelloController.java
+++ springMVC/springMVC-demo2/src/main/java/controller/HelloController.java
@@ -1,11 +1,14 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Arrays;
 
 // @RequestMapping注解：处理请求和控制器方法之间的映射关系
@@ -27,6 +30,7 @@ public class HelloController {
     @RequestMapping("/testServletAPI")
     //形参位置的request表示当前请求
     public String toTestParam(HttpServletRequest request){
+        HttpSession session = request.getSession();
         String username = request.getParameter("username");
         String pw = request.getParameter("pw");
         System.out.println("username: " +username + " pw: "+ pw);
@@ -37,10 +41,16 @@ public class HelloController {
     //若请求参数中出现多个同名的请求参数，可以在控制器方法的形参位置设置字符串类型或字符串数组来接收此请求参数
     //若使用字符串类型：最终结果为请求参数的每一个值之间使用逗号进行拼接的
     
-    public String testParam(@RequestParam(value = "user_name", required = false) String username,
-                            String pw,
-                            String[] hobby){
+    public String testParam(
+            @RequestParam(value = "user_name", required = false) String username,
+            String pw,
+            String[] hobby,
+            @RequestHeader(value = "host", required = true,defaultValue = "haha") String host,
+            //如果只有一个参数，则可以省略不写字段名，默认为Value
+            @CookieValue("JSESSIONID")String JSESSIONID){
         System.out.println("username: " +username + " pw: "+ pw + " hobby: "+ Arrays.toString(hobby));
+        System.out.println("host: " + host);
+        System.out.println("JSESSIONID: "+ JSESSIONID);
         return "success";
     }
 }
