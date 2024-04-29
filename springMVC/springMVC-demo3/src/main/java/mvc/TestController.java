d11a1f12acdb9e702b6e13e9a023e19d214505af
Cindy <siyue.Chen1997@gmail.com>
2024-04-30 01:27:23 +0800

    springMVC project startup

---
+++ springMVC/springMVC-demo3/src/main/java/mvc/TestController.java
@@ -0,0 +1,17 @@
+package mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 功能：
+ * 作者：Cindy
+ * 日期：30/04/2024 01:16
+ */
+@Controller
+public class TestController {
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
