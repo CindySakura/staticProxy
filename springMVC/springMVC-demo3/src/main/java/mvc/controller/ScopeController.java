5f062180f5d52e00eaa4212dbe087cdd84f63a4c
Cindy <siyue.Chen1997@gmail.com>
2024-05-22 23:37:39 +0800

    return of controller methods

--- springMVC/springMVC-demo3/src/main/java/mvc/controller/ScopeController.java
+++ springMVC/springMVC-demo3/src/main/java/mvc/controller/ScopeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Map;
 
 /**
@@ -52,6 +53,13 @@ public class ScopeController {
     @RequestMapping("/testModelMap")
     public String testModelMap(ModelMap modelMap){
         modelMap.addAttribute("testRequestScope","hello,ModelMap");
+        return "success";
+    }
+
+    //使用HttpSession向Session域对象共享数据
+    @RequestMapping("/testSession")
+    public String testSession(HttpSession session){
+        session.setAttribute("testSessionScope","hello,session");
         return "success";
     }
 }
