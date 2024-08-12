c5a7e974acc0115d7a83fc5395e32580bd6bbb8a
Cindy <siyue.Chen1997@gmail.com>
2024-08-12 23:30:51 +0800

    restful delete

--- springMVC/springMVC-rest/src/main/java/mvc/controller/EmployeeController.java
+++ springMVC/springMVC-rest/src/main/java/mvc/controller/EmployeeController.java
@@ -1,8 +1,14 @@
 package mvc.controller;
 
+import mvc.bean.Employee;
 import mvc.dao.EmployeeDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Collection;
 
 /**
  * 功能：
@@ -13,4 +19,11 @@ import org.springframework.stereotype.Controller;
 public class EmployeeController {
     @Autowired//默认根据byType 其次byName Resource 默认 byName, 如果byName找不到才 byType，Autowired 根据类型装配，想要根据名称需要配合 Qualifier
     private EmployeeDao employeeDao;
+
+    @RequestMapping(value="/employee", method= RequestMethod.GET)
+    public String getAllEmployee(Model model){
+        Collection<Employee> employeeList = employeeDao.getAll();
+        model.addAttribute("employeeList",employeeList);
+        return "employee_list";
+    }
 }
