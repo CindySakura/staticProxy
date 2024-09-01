1b61ff1ff003957afd60cb5462ecfcb175dd317c
Cindy <siyue.Chen1997@gmail.com>
2024-09-01 21:55:19 +0800

    interceptors

---
+++ springMVC/springMVC-demo5/src/main/java/mvc/interceptor/SecondInterceptor.java
@@ -0,0 +1,33 @@
+package mvc.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 功能：
+ * 作者：Cindy
+ * 日期：31/08/2024 00:52
+ */
+//value是bean的id/名称
+@Component(value = "secondInterceptor")
+public class SecondInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("second preHandle");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("second postHandle");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("second afterCompletion");
+    }
+}
